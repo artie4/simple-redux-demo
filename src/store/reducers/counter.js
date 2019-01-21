2a01b96905f8c31c9a8a1b0cde082fccb34d4ac5
Arthur <gaartur4@gmail.com>
2019-01-21 22:50:31 +0300

    added updateObject function for reducers

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,4 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
 
 const initialState = {
     counter: 0,
@@ -6,22 +8,14 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.INCREMENT: return {
-            ...state,
-            counter: state.counter + 1
-        }
-        case actionTypes.DECREMENT: return {
-            ...state,
-            counter: state.counter - 1
-        }
-        case actionTypes.ADD: return {
-            ...state,
-            counter: state.counter + action.value
-        }
-        case actionTypes.SUBTRACT: return {
-            ...state,
-            counter: state.counter - action.value
-        }
+        case actionTypes.INCREMENT:
+            return updateObject(state, { counter: state.counter + 1 });
+        case actionTypes.DECREMENT:
+            return updateObject(state, { counter: state.counter - 1 });
+        case actionTypes.ADD:
+            return updateObject(state, { counter: state.counter + action.value });
+        case actionTypes.SUBTRACT:
+            return updateObject(state, { counter: state.counter - action.value });
         default: break;
     }
     return state;
